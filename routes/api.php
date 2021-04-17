8eedbb4b2ecf8f72e06a2a39debe9fd701e82c10
olawale-o <omoogunolawale@gmail.com>
2021-04-17 13:02:19 +0100

    add api routes for new-gig,all-gigs, rejected-gigs,my-gigs,create and login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GigController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('new-gig', [GigController::class,'create']);
+Route::get('all-gigs', [GigController::class,'allGigs']);
+Route::get('rejected-gigs', [GigController::class,'rejectedGigs']);
+Route::get('my-gigs/{id}', [GigController::class,'gigs']);
+Route::post('create', [RegisterController::class,'create']);
+Route::post('login', [LoginController::class,'login']);
