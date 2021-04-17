f33d76b781c4e86671e5d6f6170d09a1997041fc
olawale-o <omoogunolawale@gmail.com>
2021-04-17 13:26:15 +0100

    add create, allGigs, gigs and rejectedGigs methods

---
+++ app/Http/Controllers/GigController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\NewGigFormRequest;
+use App\Models\Gig;
+use App\Models\User;
+
+class GigController extends Controller
+{
+    
+    /**
+     * create new gig
+     * 
+     * @param \Illuminate\Http\Request|request
+     * @return JsonResponse
+     */
+
+    public function create(NewGigFormRequest $request) {
+
+        $validated =  $request->validated();
+        $gig =  Gig::create([
+            'user_id' => $validated["user_id"],
+            'company' => $validated["company"],
+            'role' => $validated["role"],
+            'address' => $validated["address"],
+            'tags' => $validated["tags"],
+            'min_salary' => $validated["min_salary"],
+            'max_salary' => $validated["max_salary"],
+        ]);
+        if($gig) {
+            
+            $response = [
+                'gig' => $gig,
+                'response' => 'gig created'
+            ];
+            return response($response, 200);
+        }
+        
+        return response($response,500);
+    }
+
+    public function allGigs()
+    {
+
+        $gigs =  Gig::with("user")->get();
+
+        $response = [
+            'response' => $gigs,
+            'message' => "All gigs successfully retrieved"
+        ];
+
+        return response($response, 200);
+
+    }
+
+    public function gigs($id , Request $request)
+    {
+        $user =  User::findOrFail($id);
+        $gigs = $user->gigs;
+
+        $response = [
+            'response' => $gigs,
+            'message' => "My gigs successfully retrieved"
+        ];
+
+        return response($response, 200);
+
+    }
+
+    public function rejectedGigs()
+    {
+        $gigs =  Gig::where('is_rejected', true)->get();
+        
+
+        $response = [
+            'response' => $gigs,
+            'message' => "Rejected gigs successfully retrieved"
+        ];
+
+        return response($response, 200);
+
+    }
+}
